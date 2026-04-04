da3b11af46f80ee2f087a50b2c15e95e840149fa
SoubhagyaNeural <jainsoubhagya632@gmail.com>
2026-04-04 10:48:17 +0530

    Add solution for transformers-positional-encoding

---
+++ transformer/transformers-positional-encoding/transformers-positional-encoding.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def positional_encoding(seq_length: int, d_model: int) -> np.ndarray:
+    """
+    Generate sinusoidal positional encodings.
+    """
+    position = np.arange(seq_length).reshape(-1, 1)              # (seq_length, 1)
+    div_term = np.exp(np.arange(0, d_model, 2) * (-np.log(10000.0) / d_model))
+
+    pe = np.zeros((seq_length, d_model))
+    pe[:, 0::2] = np.sin(position * div_term)                    # even indices
+    pe[:, 1::2] = np.cos(position * div_term)                    # odd indices
+
+    return pe
